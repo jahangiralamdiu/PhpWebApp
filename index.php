bd8baa4e9c0496d96a06001cec6cecac45d77808
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-07-13 09:11:28 +0600

    inial

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <form action="index.php" method="POST">
+            ID : <input type="text" name="idText"><br/>
+            Name<input type="text" name="nameText"><br/>
+            Salary : <input type="text" name="salaryText"><br/>
+            <input type="submit" name="submitButton"><br/>
+            
+        </form>
+        <?php
+        // put your code here
+        
+        //if
+        
+        $id = $_POST['idText'];
+        $name = $_POST['nameText'];
+        $salary = $_POST['salaryText'];
+        echo $id. ' '.$name.' '.$salary;
+        
+        ?>
+    </body>
+</html>
